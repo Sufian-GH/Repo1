676b44a7053071e69a464fc8179da6bb4542fa05
Sufian Khader <sufian.khader@waldom.com>
2026-04-20 14:45:01 +0300

    Comment added

--- SQL/test1.sql
+++ SQL/test1.sql
@@ -1,3 +1,12 @@
+-- =============================================================
+-- Description : Retrieves all database objects whose SQL
+--               definition contains the phrase 'SP_Sales'.
+-- Tables Used : sys.sql_modules, sys.objects
+-- Returns     : ObjectName  - Name of the matching object
+--               ObjectType  - Descriptive type of the object
+--                             (e.g. SQL_STORED_PROCEDURE,
+--                              SQL_SCALAR_FUNCTION, VIEW, etc.)
+-- =============================================================
 SELECT o.name AS ObjectName, o.type_desc AS ObjectType
 FROM sys.sql_modules m JOIN sys.objects o
     ON m.object_id = o.object_id
